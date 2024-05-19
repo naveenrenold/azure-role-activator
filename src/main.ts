80243e4fcc2b8512f249ce1068ce3aabb51a9755
naveenrenold <naveenrenold@gmail.com>
2024-05-19 22:43:12 +0530

    dummy setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,103 @@
-import { app, BrowserWindow } from 'electron';
+import {
+    AuthenticationResult,
+  AuthorizationUrlRequest,
+  Configuration,
+  CryptoProvider,
+} from "@azure/msal-node";
+import { app, BrowserWindow, protocol, net } from "electron";
+import { PublicClientApplication } from "@azure/msal-node";
+import url from "node:url";
+import path from "node:path";
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })    
-    win.loadFile("../index.html")
+const MSAL_CONFIG: Configuration = {
+  auth: {
+    clientId: "5ad548fe-569a-465f-a98f-188af25d9b47",
+    authority:
+      "https://login.microsoftonline.com/b6281daa-0870-4760-9be1-f6b0cd37bfa7",
+  },
+};
+const scopes= "User.Read";
+const pca = new PublicClientApplication(MSAL_CONFIG);
+const redirectUri = "msal://redirect";
+const cryptoProvider = new CryptoProvider();
+const pkceCodes = {
+  challengeMethod: "S256",
+  verifier: "",
+  challenge: "",
+};
+
+interface scopesType{
+    scopes:string[]
 }
-app.whenReady().then(() => {
-    createWindow()
 
-    app.on('activate',() => {
-        if(BrowserWindow.getAllWindows().length === 0){
-            createWindow()
-        }
-    })
-})
+async function getTokenInteractive(authWindow :BrowserWindow, tokenRequest : any) : Promise<AuthenticationResult> {
+  const { verifier, challenge } = await cryptoProvider.generatePkceCodes();
+  pkceCodes.verifier = verifier;
+  pkceCodes.challenge = challenge;
 
-app.on('window-all-closed',() => {
-    if(process.platform !== 'darwin'){
-        app.quit()
+  const authCodeUrlParams: AuthorizationUrlRequest = {
+    redirectUri: redirectUri,
+    scopes: tokenRequest.scopes,
+    codeChallenge: pkceCodes.challenge,
+    codeChallengeMethod: pkceCodes.challengeMethod,
+  };
+
+  const authCodeUrl = await pca.getAuthCodeUrl(authCodeUrlParams);
+  protocol.handle(
+    redirectUri.split(":")[0],
+    (req: Request): Promise<Response> => {
+      const requestUrl = url.parse(req.url, true);
+      return net.fetch(path.normalize(`${__dirname}/${requestUrl.path}`));
     }
-})
+  );
+
+  const authCode =await listenForAuthCode(authCodeUrl, authWindow);
+
+  const authResponse =await pca.acquireTokenByCode({
+    redirectUri: redirectUri,
+    scopes: tokenRequest.scopes,
+    code: authCode ?? "",
+    codeVerifier: pkceCodes.verifier
+});
+return authResponse;
+}
+
+async  function listenForAuthCode(navigateUrl:string , authWindow: BrowserWindow) : Promise<string | null> {
+    authWindow.loadURL(navigateUrl);
+    return new Promise((resolve,reject)=>{
+        authWindow.webContents.on('will-redirect', (event, responseUrl) =>{
+            try{
+                const parsedUrl = new URL(responseUrl);
+                const authCode = parsedUrl.searchParams.get('code');
+                resolve(authCode);
+            }catch(err){
+                reject(err);
+            }
+        })
+    })
+}
+
+const createWindow = async() => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  win.loadFile("../index.html");
+  const authResult = await getTokenInteractive(win,scopes);
+  console.log(authResult.accessToken);
+};
+app.whenReady().then(() => {
+  createWindow();  
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();      
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});

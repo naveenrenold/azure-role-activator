7bfd7070cbe30785f5d1c5b804bef8e5470bc6b9
naveenrenold <naveenrenold@gmail.com>
2024-08-27 12:29:16 +0530

    gitignore

--- public/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electronAPI', {
-    getToken: (clientId, tenantId) => electron_1.ipcRenderer.invoke('getToken', clientId, tenantId)
-});
